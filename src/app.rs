794eb6cf100aa439c102fab187b5b861d5b6ca60
Karl Eklund <localpart@gmail.com>
2023-04-08 17:31:15 +0200

    Add text editor component

--- src/app.rs
+++ src/app.rs
@@ -1,11 +1,13 @@
 pub struct EditorApp {
     label: String,
+    buffer: String,
 }
 
 impl Default for EditorApp {
     fn default() -> Self {
         Self {
             label: "Hello World!".to_string(),
+            buffer: "lorem ipsum".to_string(),
         }
     }
 }
@@ -18,6 +20,10 @@ impl eframe::App for EditorApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.label(&self.label);
+            ui.add_sized(
+                ui.available_size(),
+                egui::TextEdit::multiline(&mut self.buffer).code_editor(),
+            );
         });
     }
 }
