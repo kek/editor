fbc0091a221d0dd8cabd1993ebf2ac83849f210e
Karl Eklund <localpart@gmail.com>
2023-04-10 15:02:24 +0200

    Enable switching of files

--- src/app.rs
+++ src/app.rs
@@ -2,15 +2,27 @@
 #[serde(default)]
 pub struct EditorApp {
     paths: Vec<String>,
+    active_file: String,
     #[serde(skip)]
     buffer: String,
 }
 
 impl Default for EditorApp {
     fn default() -> Self {
-        let paths = ["README.md".to_owned()].to_vec();
-        let buffer = std::fs::read_to_string(&paths[0]).unwrap();
-        Self { buffer, paths }
+        let paths = [
+            "README.md".to_owned(),
+            "src/main.rs".to_owned(),
+            "src/app.rs".to_owned(),
+            "src/lib.rs".to_owned(),
+        ]
+        .to_vec();
+        let active_file: String = paths[0].clone();
+        let buffer = std::fs::read_to_string(&active_file).unwrap();
+        Self {
+            buffer,
+            paths,
+            active_file,
+        }
     }
 }
 
@@ -19,20 +31,34 @@ impl EditorApp {
         if let Some(storage) = cc.storage {
             return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
         }
-        Default::default()
+        let mut zelf: EditorApp = Default::default();
+        zelf.buffer = std::fs::read_to_string(&zelf.active_file).unwrap();
+        zelf
+    }
+
+    fn save_active_file(&mut self) {
+        let path = self.active_file.clone();
+
+        let contents = &&self.buffer;
+        println!("saving file {:?} with contents {:?}", path, contents);
+        std::fs::write(path, contents).unwrap();
     }
 }
 
 impl eframe::App for EditorApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let path = ui.text_edit_singleline(&mut self.paths[0]);
-            if path.changed {
-                println!("path changed to {:?}", self.paths[0]);
-                match std::fs::read_to_string(&self.paths[0]) {
-                    Ok(buffer) => self.buffer = buffer,
-                    Err(err) => {
-                        eprintln!("Error: {}", err);
+            for path in self.paths.clone().into_iter() {
+                let file = ui.button(&path);
+                if file.clicked() {
+                    println!("file clicked {:?}", path);
+                    self.save_active_file();
+                    self.active_file = path;
+                    match std::fs::read_to_string(&self.active_file) {
+                        Ok(buffer) => self.buffer = buffer,
+                        Err(err) => {
+                            eprintln!("Error: {}", err);
+                        }
                     }
                 }
             }
@@ -47,7 +73,7 @@ impl eframe::App for EditorApp {
     }
 
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        std::fs::write(self.paths[0].clone(), &self.buffer).unwrap();
+        self.save_active_file();
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 }
