e37980d507af5bb28fce10f2f0cb15748583852e
Karl Eklund <localpart@gmail.com>
2023-04-10 14:36:36 +0200

    Prepare for more than one open file

--- src/app.rs
+++ src/app.rs
@@ -1,16 +1,16 @@
 #[derive(serde::Serialize, serde::Deserialize)]
 #[serde(default)]
 pub struct EditorApp {
-    path: String,
+    paths: Vec<String>,
     #[serde(skip)]
     buffer: String,
 }
 
 impl Default for EditorApp {
     fn default() -> Self {
-        let path = "README.md".to_owned();
-        let buffer = std::fs::read_to_string(&path).unwrap();
-        Self { buffer, path: path }
+        let paths = ["README.md".to_owned()].to_vec();
+        let buffer = std::fs::read_to_string(&paths[0]).unwrap();
+        Self { buffer, paths }
     }
 }
 
@@ -26,26 +26,28 @@ impl EditorApp {
 impl eframe::App for EditorApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
-            let path = ui.text_edit_singleline(&mut self.path);
+            let path = ui.text_edit_singleline(&mut self.paths[0]);
             if path.changed {
-                println!("path changed to {:?}", self.path);
-                // self.buffer = std::fs::read_to_string(&self.path).unwrap();
-                match std::fs::read_to_string(&self.path) {
+                println!("path changed to {:?}", self.paths[0]);
+                match std::fs::read_to_string(&self.paths[0]) {
                     Ok(buffer) => self.buffer = buffer,
                     Err(err) => {
                         eprintln!("Error: {}", err);
                     }
                 }
             }
-            ui.add_sized(
+            let buffer = ui.add_sized(
                 ui.available_size(),
                 egui::TextEdit::multiline(&mut self.buffer).code_editor(),
             );
+            if buffer.changed {
+                println!("buffer changed");
+            }
         });
     }
 
     fn save(&mut self, storage: &mut dyn eframe::Storage) {
-        std::fs::write(self.path.clone(), &self.buffer).unwrap();
+        std::fs::write(self.paths[0].clone(), &self.buffer).unwrap();
         eframe::set_value(storage, eframe::APP_KEY, self);
     }
 }
