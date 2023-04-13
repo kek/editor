e3a87141cedc12c77edb359cf696c09c08bd3925
Karl Eklund <localpart@gmail.com>
2023-04-13 16:40:18 +0200

    make active file optional

--- src/app.rs
+++ src/app.rs
@@ -2,7 +2,7 @@
 #[serde(default)]
 pub struct EditorApp {
     paths: Vec<String>,
-    active_file: String,
+    active_file: Option<String>,
     #[serde(skip)]
     buffer: Option<String>,
     #[serde(skip)]
@@ -13,18 +13,14 @@ pub struct EditorApp {
 
 impl Default for EditorApp {
     fn default() -> Self {
-        let paths = [
-            "README.md".to_string(),
-            "src/main.rs".to_string(),
-            "src/app.rs".to_string(),
-            "src/lib.rs".to_string(),
-        ];
+        let paths = [];
         let files = file_list();
 
         Self {
             buffer: None,
             paths: paths.to_vec(),
-            active_file: paths[0].to_owned(),
+            // active_file: Some(paths[0].to_owned()),
+            active_file: None,
             output: "".to_owned(),
             files,
         }
@@ -34,17 +30,8 @@ impl Default for EditorApp {
 fn file_list() -> Vec<std::path::PathBuf> {
     std::fs::read_dir(".")
         .unwrap()
-        .filter(|res| {
-            res.as_ref()
-                .unwrap()
-                .path()
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                != ".git".to_string()
-        })
-        .flat_map(move |res| {
+        .filter(|res| !(path_equals(res, &".git") || path_equals(res, &"target")))
+        .flat_map(|res| {
             if res.as_ref().unwrap().path().is_file() {
                 vec![res.unwrap().path()]
             } else {
@@ -59,6 +46,17 @@ fn file_list() -> Vec<std::path::PathBuf> {
         .collect()
 }
 
+fn path_equals(res: &Result<std::fs::DirEntry, std::io::Error>, name: &&str) -> bool {
+    res.as_ref()
+        .unwrap()
+        .path()
+        .file_name()
+        .unwrap()
+        .to_str()
+        .unwrap()
+        == name.to_string()
+}
+
 impl EditorApp {
     pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
         if let Some(storage) = cc.storage {
@@ -69,7 +67,7 @@ impl EditorApp {
 
     fn save_active_file(&mut self) {
         match &self.buffer {
-            Some(contents) => std::fs::write(&self.active_file, contents).unwrap(),
+            Some(contents) => std::fs::write(&self.active_file.clone().unwrap(), contents).unwrap(),
             None => println!("no buffer to save"),
         }
     }
@@ -77,21 +75,7 @@ impl EditorApp {
 
 impl eframe::App for EditorApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        self.buffer = if let Some(contents) = &self.buffer {
-            Some(contents.to_string())
-        } else {
-            let contents = match std::fs::read_to_string(&self.active_file.clone()) {
-                Ok(contents) => contents.clone(),
-                Err(err) => {
-                    eprintln!("Error: {}", err);
-                    // TODO: This does not happen when a file is externally deleted
-                    "read error".to_owned()
-                }
-            };
-            Some(contents)
-        };
         egui::SidePanel::left("my_left_panel").show(ctx, |ui| {
-            // for all self.files
             self.files.iter().for_each(|file| {
                 let path = file.as_path().to_str().unwrap();
                 let file_name = file.file_name().unwrap().to_str().unwrap();
@@ -101,19 +85,27 @@ impl eframe::App for EditorApp {
                 }
             });
         });
+
         egui::TopBottomPanel::top("my_panel").show(ctx, |ui| {
             ui.horizontal(|ui| {
                 for path in self.paths.clone().into_iter() {
-                    let button = if path == self.active_file {
-                        let button_text = egui::WidgetText::from(&path).color(egui::Color32::WHITE);
-                        egui::Button::new(button_text).fill(egui::Color32::from_rgb(150, 150, 175))
-                    } else {
-                        egui::Button::new(&path)
+                    let button = match &self.active_file {
+                        Some(active_file) => {
+                            if path == active_file.clone() {
+                                let button_text =
+                                    egui::WidgetText::from(&path).color(egui::Color32::WHITE);
+                                egui::Button::new(button_text)
+                                    .fill(egui::Color32::from_rgb(150, 150, 175))
+                            } else {
+                                egui::Button::new(&path)
+                            }
+                        }
+                        None => egui::Button::new(&path),
                     };
                     if ui.add(button).clicked() {
                         self.save_active_file();
-                        self.active_file = path;
-                        match std::fs::read_to_string(&self.active_file) {
+                        self.active_file = Some(path);
+                        match std::fs::read_to_string(&self.active_file.clone().unwrap()) {
                             Ok(buffer) => self.buffer = Some(buffer),
                             Err(err) => eprintln!("Error: {}", err),
                         }
@@ -136,27 +128,39 @@ impl eframe::App for EditorApp {
         });
 
         egui::CentralPanel::default().show(ctx, |ui| {
-            egui::ScrollArea::both().show(ui, |ui| {
-                let mut text = match &mut self.buffer {
-                    Some(buffer) => buffer,
-                    None => "empty",
-                }
-                .to_owned();
+            match &self.active_file {
+                None => {}
+                Some(path) => {
+                    self.buffer = if let Some(contents) = &self.buffer {
+                        Some(contents.to_string())
+                    } else {
+                        let contents = match std::fs::read_to_string(path) {
+                            Ok(contents) => contents.clone(),
+                            Err(err) => {
+                                eprintln!("Error: {}", err);
+                                // TODO: This does not happen when a file is externally deleted
+                                "read error".to_owned()
+                            }
+                        };
+                        Some(contents)
+                    };
+                    egui::ScrollArea::both().show(ui, |ui| {
+                        let mut text = match &mut self.buffer {
+                            Some(buffer) => buffer,
+                            None => "empty",
+                        }
+                        .to_owned();
 
-                let text_edit = egui::TextEdit::multiline(&mut text)
-                    .code_editor()
-                    .desired_width(ui.available_width());
-                if ui.add(text_edit).changed {
-                    self.buffer = Some(text);
-                    self.save_active_file();
+                        let text_edit = egui::TextEdit::multiline(&mut text)
+                            .code_editor()
+                            .desired_width(ui.available_width());
+                        if ui.add(text_edit).changed {
+                            self.buffer = Some(text);
+                            self.save_active_file();
+                        }
+                    });
                 }
-            });
-            // let mut text = match &mut self.buffer {
-            //     Some(buffer) => buffer,
-            //     None => "empty",
-            // }
-            // .to_owned();
-            // ui.text_edit_multiline(&mut text);
+            }
         });
     }
 
