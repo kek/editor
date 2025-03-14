4cc74fcd64bfb40affa7a8a3dad156310e44feab
Karl Eklund <localpart@gmail.com>
2025-03-14 22:16:26 +0100

    Upgrade dependencies so that it can compile

--- mix.exs
+++ mix.exs
@@ -25,7 +25,7 @@ defmodule Editor.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:rustler, "~> 0.27.0"},
+      {:rustler, "~> 0.36.1"},
       {:mix_test_watch, "~> 1.1", only: :dev, runtime: false},
       {:jason, "~> 1.4"},
       {:burrito, github: "burrito-elixir/burrito"}
